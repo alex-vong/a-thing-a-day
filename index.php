cf3b2940dace5d6a9c91146e245c9882abd4b89f
Alex Vong <avong003@gmail.com>
2022-04-06 12:29:22 -0700

    created index php

---
+++ index.php
@@ -0,0 +1,68 @@
+<!doctype html>
+<html class="no-js" lang="">
+
+<?php include('head.php')?>
+
+<body>
+	<header class="site-header">
+		<inner-column>
+			<?php include('site-header.php')?>
+		</inner-column>
+	</header>
+
+	<header class="hero-section">
+		<inner-column>
+			<?php include('hero.php')?>
+		</inner-column>
+	</header>
+
+	<header class="art-gallery">
+		<inner-column>
+			<?php include('art-gallery.php')?>
+		</inner-column>
+	</header>
+
+
+
+	<section class="footer">
+		<!-- <inner-column>
+			<section class="footer-container">
+				<div class="footer-text">
+					<h3 class="stout-voice">
+			  			This is the site footer and it has a "site-map" module in it.
+					</h3>
+					<p class="calm-voice">
+						Pretty standard type of thing, right?
+					</p>
+				</div>
+
+				<div class="footer-grid">
+					<section class="footer-heading">
+						<a href="" class="logo"></a>
+						<a href="" class="top">Some Page</a>
+						<a href="">Some Other Page</a>
+					</section>
+
+					<section class="footer-user">
+						<a href="">Sign-In</a>
+					</section>
+					
+					<section class="footer-legal">
+						<a href="">Legal Thing A</a>
+						<a href="">Legal Thing B</a>
+
+					</section>
+					
+					<section class="footer-social">
+						<a href="">Social Thing A</a>
+						<a href="">Social Thing B</a>
+						<a href="">Social Thing B</a>
+						<a href="">Social Thing D</a>
+					</section>
+				</div>
+			</section>
+		</inner-column> -->
+	</section>
+	
+</body>
+</html>
