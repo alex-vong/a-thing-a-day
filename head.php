62260ec541d1a26e2b4b6c9402161a0c68471e0e
Alex Vong <avong003@gmail.com>
2022-04-06 12:29:29 -0700

    added meta data

---
+++ head.php
@@ -0,0 +1,25 @@
+
+<head>
+	<meta charset="UTF-8" />
+	<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+	<title>A Thing A Day</title>
+	<meta
+		name="description"
+		content="A Thing A Day"
+	/>
+	<meta
+		name="og:description"
+		content="A Thing a Day Keeps The Derek Away"
+	/>
+
+	<meta property="og:title" content="A Thing a Day Keeps The Derek Away" />
+	<meta property="og:type" content="">
+	<meta property="og:url" content="">
+
+	<meta property="og:image" content="https://peprojects.dev/alpha-1/alexvong/images/og-image.png" />
+	<link rel="shortcut icon" type="image/jpg" href="[[favicon]]" />
+
+	<link rel="stylesheet" href="css/styles.css" />
+
+</head>
+
