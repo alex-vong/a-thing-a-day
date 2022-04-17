e720123cdd8cc97a47b7fe59a102d399745879aa
Alex Vong <avong003@gmail.com>
2022-04-16 21:25:23 -0700

    added day 6-10

---
+++ art-factory.php
@@ -0,0 +1,68 @@
+<?php 
+
+$gallery = [
+	[
+		"id" => 1,
+		"day" => 1,
+		"piece" => 'images/day-1.svg',
+	],
+	[
+		"id" => 2,
+		"day" => 2,
+		"piece" => 'images/day-2.svg',
+	],
+	[
+		"id" => 3,
+		"day" => 3,
+		"piece" => 'images/day-3.svg',
+	],
+	[
+		"id" => 4,
+		"day" => 4,
+		"piece" => 'images/day-4.svg',
+	],
+	[
+		"id" => 5,
+		"day" => 5,
+		"piece" => 'images/day-5.svg',
+	],
+	[
+		"id" => 6,
+		"day" => 6,
+		"piece" => 'images/day-6.svg',
+	],
+	[
+		"id" => 7,
+		"day" => 7,
+		"piece" => 'images/day-7.svg',
+	],
+
+	[
+		"id" => 8,
+		"day" => 8,
+		"piece" => 'images/day-8.svg',
+	],
+	
+	[
+		"id" => 9,
+		"day" => 9,
+		"piece" => 'images/day-9.svg',
+	],
+
+	[
+		"id" => 10,
+		"day" => 10,
+		"piece" => 'images/day-10.svg',
+	]
+
+
+]
+
+
+
+
+
+
+
+
+ ?>
