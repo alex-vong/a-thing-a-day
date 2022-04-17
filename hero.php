b19e4a148eed73629cb0125521eddc6a3d6fb0e2
Alex Vong <avong003@gmail.com>
2022-04-16 21:25:53 -0700

    updated jero

---
+++ hero.php
@@ -0,0 +1,37 @@
+<hero-stuff>
+	<h1 class="loud-voice">MoAA - Museum of Alex Art</h1>
+
+	<section class="hero-monologue">
+
+		<div class="about">
+			<h3 class="stout-voice">About</h3>
+			<p class="calm-voice">Inspired by this <a href="https://perpetual.education/exercises/thing-a-day/">article</a> written by Derek from Perertual Education's "Design for the Web" Mentorship</p>
+		</div>
+
+		<div class="rules">
+			<h3 class="stout-voice">Rules</h3>
+			<p class="calm-voice">Just make a thing a day. That's it! Oh and don't give up!</p>
+		</div>
+
+		<!-- <div class="goals">
+		<h3 class="stout-voice">My Goals</h3>
+			<ul>
+				<li class="whisper">Theme? I like skulls so it will be skull related!</li>
+				<li class="whisper">Paper and Pencil? Pen and Pad? Stone and chizel (is that how you spell it? I guess I'll used Affinity Designer to try and get better at it </li>
+				<li class="whisper">30 Minutes a day time boxing</li>
+				<li class="whisper">Practicing using AD tools and concepts like
+					<ul>
+						<li>Pen, Brush, Node, Shapes</li>
+						<li>Stroke</li>
+						<li>Brushes</li>
+						<li>Colors, Shadows, Gradients</li>
+						<li>Layers, Nested Objects, Grouping</li>
+						<li>and many more!</li>
+					</ul>
+				</li>
+				<li class="whisper">Finding 30 minutes a day TO DO this challenge</li>
+			</ul>
+		</div> -->
+
+	</section>
+</hero-stuff>
---
+++ site-header.php
@@ -0,0 +1,11 @@
+<masthead>
+	<nav class="site-menu">
+		<a href="" class="logo">
+			<picture>
+				<img src="images/no-face-logo.svg" alt="">
+			</picture>
+		</a>
+		<h2 class="stern-voice">A Thing a Day</h2>
+</masthead>
+
+
